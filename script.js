696f11daa18417ab1446b26a780ed52c7dcea96f
Christina Larsen <clarsen1782@outlook.com>
2023-09-27 12:08:26 -0700

    Psuedo Code for the script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+//set varables to win/lose/tie to Zero
+
+//Set Prompt to ask user to choose r-p-s
+
+//Computer ramdom slect r-p-s - Create function for the automated responce
+
+//eval user responce vs computer responce
+
+/*if-else statment for win/lose/tie conditions
+r>s s>p p>r Tie - Win formulas 
+s<r p<s r<p - lose formulas*/
+
+//log user wins to computer wins
+
+//based on results incurment the varible by 1
+
+//Display results with the alert
+
+/*prompt user to play again
+    if yes, begin again
+    if no, display "thank you come back later"*/
